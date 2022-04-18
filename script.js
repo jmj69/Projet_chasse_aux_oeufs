327aadab8a9786ac387fae34444e0aceaa788682
Jacquier jean-Marie <jm.jacquier@gmail.com>
2022-04-18 15:03:55 +0200

    menu burger

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const sidenav = document.getElementById("mySidenav");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
 
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+function closeNav() {
+  sidenav.classList.remove("active");
+}
